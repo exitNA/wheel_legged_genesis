b661e05725d41d8d1a777726e06b167dba8194e9
albusgive <mb18743920955@163.com>
2025-03-13 10:24:28 +0800

    修改：     sim2real/motor_test/mi_test.cpp

--- sim2real/motor_test/mi_test.cpp
+++ sim2real/motor_test/mi_test.cpp
@@ -1,5 +1,5 @@
-#include "PCAN.hpp"
 #include "MiMotor.h"
+#include "PCAN.hpp"
 int main() {
   PCAN pcan;
   auto channel = CAN1;
@@ -13,28 +13,29 @@ int main() {
   TPCANMsg motor_enable = *mi.enableMotor(motor_id, true);
   pcan.send(channel, motor_enable);
 
-  auto loc = *mi.locomotion(motor_id, 0.0, 0.0, 1.0, 0.0, 0.1);
-  pcan.send(channel, loc);
+  // auto loc = *mi.locomotion(motor_id, 0.0, 0.0, 1.0, 0.0, 0.1);
+  // pcan.send(channel, loc);
 
-  //   TPCANMsg send_msg = mi.set_parameter(motor_id, motor_indexs::spd_ref, 5.2);
-  //   pcan.send(channel, send_msg);
+  auto send_msg = *mi.set_parameter(motor_id, motor_indexs::spd_ref, 2.0);
+  send_msg.print();
+  pcan.send(channel, send_msg);
 
   //   send_msg = mi.set_parameter(motor_id, motor_indexs::limit_cur, 23);
   //   pcan.send(channel, send_msg);
   //   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-  for(int i=0;i<100;i++){
+  for (int i = 0; i < 50; i++) {
     auto [is_read, can_msg] = pcan.read(channel);
     if (is_read) {
-        // MiCANMsg(can_msg).print();
-        auto decode = mi.decode(can_msg);
-        decode.print();
-        // decode.invertMotor()->print();
+      // MiCANMsg(can_msg).print();
+      // auto decode = mi.decode(can_msg);
+      // decode.print();
+      // decode.invertMotor()->print();
     }
-    pcan.send(channel, loc);
+    // pcan.send(channel, loc);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
-  
+
   pcan.send(channel, *mi.enableMotor(motor_id, false));
   return 0;
 }
